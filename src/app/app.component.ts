af4b3974f6d7449d6db9a74ecace9831eb5273f6
Andy <andy.french@hotmail.co.uk>
2019-11-14 22:35:20 +0000

    Moved todos to javascript and added a toogle event.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+type Todo = {
+  id: number;
+  text: string;
+  isDeleted: boolean;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular';
+
+  todos: Todo[] = [
+    {
+      id: 1,
+      text: 'Walk the dog',
+      isDeleted: false
+    },
+    {
+      id: 2,
+      text: 'Got to the gym',
+      isDeleted: false
+    },
+    {
+      id: 3,
+      text: 'get the paper',
+      isDeleted: false
+    },
+    {
+      id: 4,
+      text: 'string',
+      isDeleted: false
+    },
+    {
+      id: 5,
+      text: 'bar',
+      isDeleted: false
+    }
+  ];
+
+  delete(id) {
+    for (let todo of this.todos) {
+      if (todo.id === id) {
+        todo.isDeleted = !todo.isDeleted;
+      }
+    }
+  }
 }
