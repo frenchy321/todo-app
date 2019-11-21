65594ee6fe37b839e793d5d5f70a01a9a40ac151
Andy <andy.french@hotmail.co.uk>
2019-11-21 20:18:01 +0000

    Renamed deleted to completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,66 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 type Todo = {
   id: number;
   text: string;
-  isDeleted: boolean;
+  isCompleted: boolean;
+  delTodos: boolean;
 };
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'todo-angular';
+  title = "todo-angular";
 
   todos: Todo[] = [
     {
       id: 1,
-      text: 'Walk the dog',
-      isDeleted: false
+      text: "Walk the dog",
+      isCompleted: false,
+      delTodos: false
     },
     {
       id: 2,
-      text: 'Got to the gym',
-      isDeleted: false
+      text: "Got to the gym",
+      isCompleted: false,
+      delTodos: false
     },
     {
       id: 3,
-      text: 'get the paper',
-      isDeleted: false
+      text: "get the paper",
+      isCompleted: false,
+      delTodos: false
     },
     {
       id: 4,
-      text: 'string',
-      isDeleted: false
+      text: "string",
+      isCompleted: false,
+      delTodos: false
     },
     {
       id: 5,
-      text: 'bar',
-      isDeleted: false
+      text: "bar",
+      isCompleted: false,
+      delTodos: false
     }
   ];
 
-  delete(id) {
+  markAsComplete(id) {
     for (let todo of this.todos) {
       if (todo.id === id) {
-        todo.isDeleted = !todo.isDeleted;
+        todo.isCompleted = !todo.isCompleted;
       }
     }
   }
+
+  // markAsDelete(todos) {
+  //   for (let todo of this.todos) {
+  //     if (todo.id === id) {
+  //       todo.isDeleted = !todo.isDeleted;
+  //     }
+  //   }
+  // }
 }
